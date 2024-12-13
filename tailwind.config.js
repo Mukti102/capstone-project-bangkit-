9b61951202e023017822be1bc5c2cb8bf69c28d9
unknown <abdulmukti40390@gmail.comgit config --global.user.mukti>
2024-12-13 09:39:10 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 
-import flowbite from "flowbite/plugin";
+const flowbite = require("flowbite-react/tailwind");
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -15,7 +17,7 @@ export default {
     },
   },
   plugins: [
-    flowbite,
+    flowbite.plugin(),
   ],
 }
 
